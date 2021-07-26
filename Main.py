6e6ef855853b499bf6c9ec92fb7bc80909ccefae
CodeMonk643 <87682690+CodeMonk643@users.noreply.github.com>
2021-07-26 16:10:11 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,5 +6,5 @@ ph_no = input('Enter phone number with Country Code: ')
 a = phonenumbers.parse(ph_no)
 
 # Print Details
-service = carrier.name_to_number(a, 'en')
+service = carrier.name_for_number(a, 'en')
 print(service)
